490c459f7f2f0bf66c956397e75bd2d0c394004b
Jose2216 <145219166+Jose2216@users.noreply.github.com>
2024-08-01 19:41:21 -0600

    Semanas 8 y 9
    fdsfsdfdfsdfsdfd

--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -1,19 +1,19 @@
-
 package com.tienda.service;
 
 import com.tienda.domain.Producto;
 import java.util.List;
 
-
 public interface ProductoService {
-    
+
     //Se obtiene un listado de productos en un List
-    public List <Producto> getProductos(boolean activos);
-    
+    public List<Producto> getProductos(boolean activos);
+
     public Producto getProducto(Producto producto);
-    
+
     public void save(Producto producto);
-    
+
     public void delete(Producto producto);
-    
+
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+
 }
